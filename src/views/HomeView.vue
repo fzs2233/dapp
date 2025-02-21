5660ec117b20f535f9b30803af801479531db38a
fzs <2518816287>
2025-02-21 15:02:25 +0800

    搭建一个新项目dapp

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import Web3 from 'web3'
+const string = ref('hello web3')
+
+var web3 = new Web3(Web3.givenProvider || '')
+</script>
+
+<template>
+  <main>
+    <h1>{{ string }}</h1>
+    <el-button type="primary">转账</el-button>
+  </main>
+</template>
+
+<style scoped lang="less">
+h1 {
+  color: red;
+}
+</style>
